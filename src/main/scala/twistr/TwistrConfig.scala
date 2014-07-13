4797e2d8b2f62002a29398966e659b47a23146fc
Henning Schmiedehausen <henning@schmiedehausen.org>
2014-07-12 18:53:32 -0700

    Tweaks for Kafka 0.7.2

--- src/main/scala/twistr/TwistrConfig.scala
+++ src/main/scala/twistr/TwistrConfig.scala
@@ -1,6 +1,17 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package twistr
-
-;
 
 import org.skife.config.Config;
 
@@ -16,7 +27,7 @@ trait TwistrConfig {
 
   @Config(Array("twistr.access-token-secret"))
   def getAccessTokenSecret(): String
-  
-  @Config(Array("twistr.kafka.brokers"))
-  def getBrokers(): String
+
+  @Config(Array("twistr.kafka.zookeeper"))
+  def getZookeeper(): String
 }
